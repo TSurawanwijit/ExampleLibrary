d47452b2c5eca50b7041ba34ae25ebf623f39c94
thianchai M1 <thianchai@ltman.com>
2024-10-15 03:02:40 +0700

    update version from 1.0.19 to 1.0.20

--- libraryA/build.gradle.kts
+++ libraryA/build.gradle.kts
@@ -41,7 +41,7 @@ publishing {
         register<MavenPublication>("libraryARelease") {
             groupId = "com.github.tsurawanwijit"
             artifactId = "examplelibrary"
-            version = "1.0.19"
+            version = "1.0.20"
 
             afterEvaluate {
                 from(components["release"])
